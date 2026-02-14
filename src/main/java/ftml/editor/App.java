4b995f017cca109e10d2afab255caed1f1901f75
secretppl608 <lacak3736@gmail.com>
2026-02-14 23:22:42 +0800

    0.0.4

--- src/main/java/ftml/editor/App.java
+++ src/main/java/ftml/editor/App.java
@@ -40,6 +40,7 @@ public class App extends Application {
         });
         ii.setOnMouseClicked((e)->{
             h.hb(ii);
+
         });
         ss.setOnMouseClicked((e)->{
             h.hb(ss);
