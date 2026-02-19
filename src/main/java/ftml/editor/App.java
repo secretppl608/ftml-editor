6b3478bbc42bc6af57fedf3dacbe4d273f38277b
secretppl608 <lacak3736@gmail.com>
2026-02-19 16:46:10 +0800

    0.1.0-beta

--- src/main/java/ftml/editor/App.java
+++ src/main/java/ftml/editor/App.java
@@ -3,20 +3,39 @@ package ftml.editor;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 import javax.swing.*;
+
+import me.friwi.jcefmaven.CefAppBuilder;
+import me.friwi.jcefmaven.CefInitializationException;
+import me.friwi.jcefmaven.MavenCefAppHandlerAdapter;
+import me.friwi.jcefmaven.UnsupportedPlatformException;
+import me.friwi.jcefmaven.impl.progress.ConsoleProgressHandler;
+import org.cef.CefApp;
+import org.cef.CefClient;
+import org.cef.browser.CefBrowser;
+import org.cef.handler.CefDisplayHandlerAdapter;
+import org.cef.handler.CefLoadHandlerAdapter;
 import org.jetbrains.annotations.NotNull;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import ftml.editor.util.*;
 
 public class App {
     private static int panelWidth;
     private static final Color baseColor = new Color(0xE4E2E3);
+    private static String loadUrl = "";
     public static void main(String[] args) throws Exception {
         FlatMacLightLaf.setup();
         JFrame frame = new JFrame("Wikitext Editor");
         JFrame.setDefaultLookAndFeelDecorated(true);
         UIManager.put("Button.border", BorderFactory.createEmptyBorder(5, 5, 5, 5));
         ImageIcon icon = new ImageIcon(Objects.requireNonNull(App.class.getResource("/assets/ftml-icon.jpg")));
+
+        loadUrl = Objects.requireNonNull(App.class.getResource("/web/show.html")).toURI().toString();
         Image image = icon.getImage();
         frame.setIconImage(image);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,11 +64,18 @@ public class App {
         doSomething(panel);
 
         createTopArea(panel,window);
+        originGod_StartUp(window);
 
         window.setBackground(baseColor);
         window.setOpaque(true);
         frame.add(window);
         frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                CefApp.getInstance().dispose();
+            }
+        });
     }
 
     // $ ~ main
@@ -83,7 +109,7 @@ public class App {
         JPanel topArea = new JPanel();
         topArea.setBackground(baseColor);
         topArea.setLayout(new BorderLayout());
-        topArea.setPreferredSize(new Dimension(panelWidth, 170));
+        topArea.setPreferredSize(new Dimension(panelWidth, 120));
         topArea.setOpaque(false);
         topArea.add(panel,BorderLayout.NORTH);
         createTopAreaInnerControlBoxOuterWhiteBox(topArea);
@@ -92,11 +118,11 @@ public class App {
 
     // ? need param <= ToolBox.java constructor.getPanel()
     private static void createTopAreaInnerControlBoxOuterWhiteBox(JPanel parentPanel) {
-        JPanel topAreaInnerControlBoxOuterWhiteBox =  new JPanel();
+        JPanel topAreaInnerControlBoxOuterWhiteBox =  new RoundBorderPanel();
         topAreaInnerControlBoxOuterWhiteBox.setBackground(Color.WHITE);
         topAreaInnerControlBoxOuterWhiteBox.setOpaque(true);
         // todo need child panel addTo $
-        topAreaInnerControlBoxOuterWhiteBox.setPreferredSize(new Dimension(panelWidth, 150));
+        topAreaInnerControlBoxOuterWhiteBox.setPreferredSize(new Dimension(panelWidth, 100));
         var a = new JPanel();
         var b = new JPanel();
         b.setOpaque(false);
@@ -105,6 +131,27 @@ public class App {
         parentPanel.add(b, BorderLayout.WEST);
         topAreaInnerControlBoxOuterWhiteBox.setBorder(BorderFactory.createLineBorder(Color.black,1,true));
         parentPanel.add(topAreaInnerControlBoxOuterWhiteBox, BorderLayout.CENTER);
+    }
+
+    // 原神，启动！！！
+    private static void originGod_StartUp(JPanel parentPanel) throws UnsupportedPlatformException, CefInitializationException, IOException, InterruptedException {
+        System.out.println("原神，启动！");
+        //Create a new CefAppBuilder instance
+        CefAppBuilder builder = new CefAppBuilder();
+
+//Configure the builder instance
+        builder.setInstallDir(new File("jcef-bundle")); //Default
+        builder.setProgressHandler(new ConsoleProgressHandler()); //Default
+        builder.addJcefArgs("--disable-gpu"); //Just an example
+        builder.getCefSettings().windowless_rendering_enabled = false; //Default - select OSR mode
+//Set an app handler. Do not use CefApp.addAppHandler(...), it will break your code on macOS!
+        builder.setAppHandler(new MavenCefAppHandlerAdapter(){});
+//Build a CefApp instance using the configuration above
+        CefApp cefApp = builder.build();
+        CefClient client = cefApp.createClient();
+
+        CefBrowser browser = client.createBrowser(loadUrl, false, false);
+        parentPanel.add((Component) browser.getUIComponent(),BorderLayout.CENTER);
     }
 }
 
--- src/main/java/ftml/editor/util/RoundBorderPanel.java
+++ src/main/java/ftml/editor/util/RoundBorderPanel.java
@@ -8,12 +8,27 @@ public class RoundBorderPanel extends JPanel {
         super();
         setOpaque(false);
     }
+
     public RoundBorderPanel(LayoutManager layoutManager){
         super(layoutManager);
         setOpaque(false);
     }
+
     @Override
     protected void paintBorder(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.gray);
+        g2.drawRoundRect(0,0,getWidth()-1,getHeight()-1,20,20);
+        g2.dispose();
+    }
 
+    @Override
+    protected void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0,0,getWidth()-1,getHeight()-1,20,20);
+        g2.dispose();
     }
 }
